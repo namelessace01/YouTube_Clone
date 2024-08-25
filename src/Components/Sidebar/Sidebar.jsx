bbab716011632c6a73038cce72382491dd641436
namelessace01 <adedamolaajala33@gmail.com>
2024-08-25 16:45:11 +0100

    Sidebar Loading

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -10,15 +10,46 @@ import tech from '../../assets/tech.png'
 import music from '../../assets/music.png'
 import blogs from '../../assets/blogs.png'
 import news from '../../assets/news.png'
-import jack from '../../assets/jack.png'
-import simon from '../../assets/simon.png'
-import tom from '../../assets/tom.png'
-import megan from '../../assets/megan.png'
-import cameron from '../../assets/cameron.png'
+// import jack from '../../assets/jack.png'
+// import simon from '../../assets/simon.png'
+// import tom from '../../assets/tom.png'
+// import megan from '../../assets/megan.png'
+// import cameron from '../../assets/cameron.png'
 
 const Sidebar = () => {
     return (
-        <div></div>
+        <div className="sidebar">
+            <div className="sortcut-link">
+                <div className="side-link">
+                    <img src={home} alt=""/><p>Home</p>
+                </div>
+                <div className="side-link">
+                    <img src={game_icon} alt=""/><p>Gaming</p>
+                </div>
+                <div className="side-link">
+                    <img src={automobiles} alt=""/><p>Automobiles</p>
+                </div>
+                <div className="side-link">
+                    <img src={sports} alt=""/><p>Sports</p>
+                </div>
+                <div className="side-link">
+                    <img src={entertainment} alt=""/><p>Entertainment</p>
+                </div>
+                <div className="side-link">
+                    <img src={tech} alt=""/><p>Technology</p>
+                </div>
+                <div className="side-link">
+                    <img src={music} alt=""/><p>Music</p>
+                </div>
+                <div className="side-link">
+                    <img src={blogs} alt=""/><p>Blogs</p>
+                </div>
+                <div className="side-link">
+                    <img src={news} alt=""/><p>News</p>
+                </div>
+                <hr />
+            </div>
+        </div>
     )
 }
 
